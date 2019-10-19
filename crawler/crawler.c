093563e937804e887ca00b140fe9fa4780201699
CyMachEngs50 <cynthiagreg19f@gmail.com>
2019-10-19 15:22:03 -0400

    update

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,11 +1,20 @@
-//simple hello file
-#include <stdio.h>
-#include <string.h>
-
-
-int main(void){
-
-  printf("hello\n");
-  return 0;
-
-}
+/* crawler.c --- 
+ * 
+ * 
+ * Author: Gregory Macharia and Cynthia Jeruto
+ * Created: Sat Oct 19 15:12:25 2019 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+#include <stdio.h>                                                                                                                             
+#include <string.h>                                                                                                                            
+                                                                                                                                               
+                                                                                                                                               
+int main(void){                                                                                                                                
+                                                                                                                                               
+  printf("hello\n");                                                                                                                           
+  return 0;                                                                                                                                    
+                                                                                                                                               
+}      
