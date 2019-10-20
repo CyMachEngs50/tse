781d92aafd9c0c7712df1ab68411b4784ca5686b
CyMaChEngs50 <cynthiagreg19f@gmail.com>
2019-10-19 22:18:06 -0400

    step 2 done, valgrind error

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -1,11 +1,35 @@
 //simple hello file
 #include <stdio.h>
 #include <string.h>
+#include "webpage.h"
 
 
 int main(void){
-
+  
   printf("hello\n");
-  return 0;
-
+  
+  char *urlp = "https://thayer.github.io/engs50/";
+  webpage_t *webpage1 = webpage_new(urlp, 0, NULL);
+  bool p = webpage_fetch(webpage1);
+  int pos=0;
+  char *result;
+  
+  if (!p){ 		      				    
+    exit(EXIT_FAILURE);
+  }
+ 
+  while((pos = webpage_getNextURL(webpage1, pos, &result))>=0){
+    if(IsInternalURL(result)){
+      printf("internal url: %s\n", result);
+      free(result);
+    }
+    else{
+      printf("external url: %s\n", result);
+      free (result);
+      }
+  }
+  
+  free(webpage_getHTML(webpage1));
+  free(webpage1);
+  exit(EXIT_SUCCESS);
 }
