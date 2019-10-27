26241682f9c79f5fe08ac4318ca825d340de9e74
CyMaChEngs50 <cynthiagreg19f@gmail.com>
2019-10-27 16:11:27 -0400

    pageio.c initial commit.

--- utils/pageio.c
+++ utils/pageio.c
@@ -1,10 +1,31 @@
-/* pageio.c --- 
+/* pageio.c --- implements pagesave and pageload
  * 
  * 
- * Author: Gregory G. Macharia
+ * Author: Gregory G. Macharia & Cynthia Bundi
  * Created: Sun Oct 27 15:38:19 2019 (-0400)
- * Version: 
+ * Version: 1.0
  * 
  * Description: 
  * 
  */
+
+
+#include <inttypes.h>
+#include "pageio.h"
+
+FILE *fp;
+char *pathname;
+int32_t pagesave(webpage_t *pagep, int id, char *dirname){
+	
+  char *urlp= webpage_getURL(pagep);
+  char *htmlp= webpage_getHTML(pagep);
+  int depth= webpage_getDepth(pagep);
+  int HTMLlen= webpage_getHTMLlen(pagep);
+
+  sprintf(pathname, "%s%d",dirname, id);
+  fp=fopen(pathname, "w+");
+  fprintf(fp, " %s\n %d\n %d\n %s", urlp, depth, HTMLlen, htmlp);
+  fclose(fp);
+}
+
+ 
